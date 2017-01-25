9c6204c0b8a4b1c43ece837c039efee0787b9f03
Elias Alves <unixelias@gmail.com>
2017-01-25 15:58:37 -0200

    Add ldap e configurações travis

--- repo_build.sh
+++ repo_build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -H 'Content-Type: application/json' --data '{"build": true}' -X POST https://registry.hub.docker.com/u/caduvieira/plonegovbridg/trigger/$TRIGGER_TOKEN/
+curl -H 'Content-Type: application/json' --data '{"build": true}' -X POST https://registry.hub.docker.com/u/unixelias/portal.docker/trigger/$TRIGGER_TOKEN/
