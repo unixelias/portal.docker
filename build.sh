f5863902970144abfffd59294327e7466ff60248
Elias Alves <unixelias@gmail.com>
2017-01-28 16:30:31 -0200

    Ajustes de configuração

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ shopt -s extglob
 for i in $( ls docker); do
   case "$i" in
        *-test )  ;;
-       * ) docker build -t unixelias/portal.docker:$i docker/$i ;;
+       * ) docker build -t unixelias/plone.idg:$i docker/$i ;;
   esac
 done
