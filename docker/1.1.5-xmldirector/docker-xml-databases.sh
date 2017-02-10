6d1fc0912efa0828ffb2483db3d7f395f49cac4c
Elias Alves <unixelias@gmail.com>
2017-02-10 12:45:34 -0200

    xmldirector 0.1

--- docker/1.1.5-xmldirector/docker-xml-databases.sh
+++
@@ -1,8 +0,0 @@
-docker stop basex
-docker stop existdb
-docker kill basex
-docker kill existdb
-docker rm basex
-docker rm existdb
-docker run -d -p 22080:8984 --name basex zopyx/basex-80
-docker run -d -p 22081:8080 --name existdb zopyx/existdb-22
