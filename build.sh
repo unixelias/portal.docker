093e67e76aa5a314555e83de9219e0e95098837e
Elias Alves <unixelias@gmail.com>
2017-02-17 23:21:09 -0200

    Correção travis

--- build.sh
+++ build.sh
@@ -1,11 +1,9 @@
- #!/bin/bash
+#!/bin/bash
 shopt -s extglob
 
-#for i in $( ls docker); do
-#  case "$i" in
-#       *-test )  ;;
-#       * ) docker build -t unixelias/plone.idg:$i docker/$i ;;
-#  esac
-#done
-
-docker build -t unixelias/plone.idg:1.1.5-x docker/1.1.5-xmldirector
+for i in $( ls docker); do
+  case "$i" in
+       *-test )  ;;
+       * ) docker build -t unixelias/plone.idg:$i docker/$i ;;
+  esac
+done
