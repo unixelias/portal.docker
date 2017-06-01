776b2bb68f1b767e99a4f5cc5a792df63a0325f2
Elias Alves <unixelias@gmail.com>
2017-05-31 22:48:17 -0300

    Limpa temas
    Ajusta sistema de push do travis para o hub.docker

--- build.sh
+++ build.sh
@@ -4,7 +4,6 @@ shopt -s extglob
 for i in $( ls docker); do
   case "$i" in
        *-test )  ;;
-       * ) docker build -t unixelias/plone.idg:$i docker/$i ;;
+       * ) docker build -t unixelias/plone.idg:$i docker/$i && docker push unixelias/plone.idg:$i ;;
   esac
 done
-
