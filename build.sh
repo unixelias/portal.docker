8df595e77ef93657c55ae9eca4f73a82d3fb6939
Elias Alves <unixelias@gmail.com>
2017-08-10 12:21:01 -0300

    Ajuste de tags

--- build.sh
+++ build.sh
@@ -4,8 +4,14 @@ shopt -s extglob
 #for i in $( ls docker); do
 #  case "$i" in
 #       *-test )  ;;
-#       * ) docker build -t unixelias/plone.idg:$i docker/$i ;;
+#       * ) docker build -t unixelias/plone.idg:$i \
+#                    --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
+#                    --build-arg VCS_REF=`git rev-parse --short HEAD` docker/$i \
+#                    && docker push unixelias/plone.idg:$i ;;
 #  esac
 #done
 
-docker build -t unixelias/plone.idg:1.1.5-dev docker/1.1.5-dev && docker push unixelias/plone.idg:1.1.5-dev
+docker build -t unixelias/plone.idg:1.1.5-dev \
+        --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
+        --build-arg VCS_REF=`git rev-parse --short HEAD` docker/1.1.5-dev \
+        && docker push unixelias/plone.idg:1.1.5-dev
