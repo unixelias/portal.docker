03e9750fff0855078205a004f0828deb02868b0f
Elias Alves <unixelias@gmail.com>
2017-08-10 13:20:18 -0300

    Revert "Refatoração e troca do proxy"
    This reverts commit d79c466d192f643395ea26cbd034b873cf4ff6ce.

--- build.sh
+++ build.sh
@@ -1,17 +1,9 @@
- #!/bin/bash
+#!/bin/bash
 shopt -s extglob
 
 for i in $( ls docker); do
- case "$i" in
-      *-test )  ;;
-      * ) docker build -t unixelias/plone.idg:$i \
-                   --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
-                   --build-arg VCS_REF=`git rev-parse --short HEAD` docker/$i \
-                   && docker push unixelias/plone.idg:$i ;;
- esac
+  case "$i" in
+       *-test )  ;;
+       * ) docker build -t unixelias/plone.idg:$i docker/$i && docker push unixelias/plone.idg:$i ;;
+  esac
 done
-
-# docker build -t unixelias/plone.idg:1.1.5-dev \
-#         --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
-#         --build-arg VCS_REF=`git rev-parse --short HEAD` docker/1.1.5-dev \
-#         && docker push unixelias/plone.idg:1.1.5-dev
--- repo_build.sh
+++ repo_build.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-# curl -H 'Content-Type: application/json' --data '{"build": true}' -X POST https://registry.hub.docker.com/u/unixelias/plone.idg/trigger/$TRIGGER_TOKEN/
-curl -X POST https://hooks.microbadger.com/images/unixelias/plone.idg/UqI2jjYcxbC3LKRwYp90UHNiNZE=
+curl -H 'Content-Type: application/json' --data '{"build": true}' -X POST https://registry.hub.docker.com/u/unixelias/plone.idg/trigger/$TRIGGER_TOKEN/
