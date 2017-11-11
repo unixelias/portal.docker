6df0df4f62b660cb700b99c58fde331e031d4a48
Elias Alves <unixelias@gmail.com>
2017-11-11 11:05:54 -0200

    Atualiza tags

--- build.sh
+++ build.sh
@@ -1,17 +1,16 @@
  #!/bin/bash
 shopt -s extglob
 
-#for i in $( ls docker); do
-#  case "$i" in
-#       *-test )  ;;
-#       * ) docker build -t unixelias/plone.idg:$i \
-#                    --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
-#                    --build-arg VCS_REF=`git rev-parse --short HEAD` docker/$i \
-#                    && docker push unixelias/plone.idg:$i ;;
-#  esac
-#done
+for i in $( ls docker); do
+ case "$i" in
+      dev ) docker build -t unixelias/plone.idg:$i \
+                   --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
+                   --build-arg VCS_REF=`git rev-parse --short HEAD` docker/$i \
+                   && docker push unixelias/plone.idg:$i ;;
+ esac
+done
 
-docker build -t unixelias/plone.idg:dev \
-        --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
-        --build-arg VCS_REF=`git rev-parse --short HEAD` docker/dev \
-        && docker push unixelias/plone.idg:dev
+# docker build -t unixelias/plone.idg:dev \
+#         --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
+#         --build-arg VCS_REF=`git rev-parse --short HEAD` docker/dev \
+#         && docker push unixelias/plone.idg:dev
