cbcaa20d85c011c18019a7cface9947b49807497
Elias Alves <unixelias@users.noreply.github.com>
2017-11-20 17:48:53 -0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ last=""
 
 for i in $( ls docker); do
  case "$i" in
-      *-test )  ;;
-      *1.5|dev ) docker build -t unixelias/plone.idg:$i \
+      *-test|dev  )  ;;
+      *) docker build -t unixelias/plone.idg:$i \
                    --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
                    --build-arg VCS_REF=`git rev-parse --short HEAD` docker/$i \
                    && docker push unixelias/plone.idg:$i \
