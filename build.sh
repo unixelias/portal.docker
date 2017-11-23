3daa311acd701b91b3d3b5408c9f64cff7a0b0f5
Elias Alves <unixelias@gmail.com>
2017-11-23 13:21:53 -0200

    Testa pacotes adcionais

--- build.sh
+++ build.sh
@@ -7,6 +7,10 @@ for i in $( ls docker); do
                    --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
                    --build-arg VCS_REF=`git rev-parse --short HEAD` docker/$i \
                    && docker push unixelias/plone.idg:$i ;;
+      1.1.5.3 ) docker build -t unixelias/plone.idg:1.1.5.3-dev \
+                  --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
+                  --build-arg VCS_REF=`git rev-parse --short HEAD` docker/$i \
+                  && docker push unixelias/plone.idg:1.1.5.3-dev ;;
  esac
 done
 
