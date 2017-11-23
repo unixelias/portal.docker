5e3c237f80a372eb549d59ec967ac630ff5557e3
Elias Alves <unixelias@gmail.com>
2017-11-23 11:48:26 -0200

    Adiciona novo registro
    Novas tags em tempo de compilação e envio para registro privado
    Uniformização de plugins
    Tratamento de volumes externo ao compose
    Resgate de versões antigas

--- build.sh
+++ build.sh
@@ -14,7 +14,11 @@ for i in $( ls docker); do
  esac
 done
 
-docker tag unixelias/plone.idg:$last unixelias/plone.idg:latest && docker push unixelias/plone.idg:latest
+docker tag unixelias/plone.idg:$last unixelias/plone.idg:latest && docker push unixelias/plone.idg:latest;
+
+docker login -u="$DOCKER_UFVJM_USERNAME" -p="$DOCKER_UFVJM_PASSWORD" hub.dds.ufvjm.edu.br;
+docker tag unixelias/plone.idg:latest hub.dds.ufvjm.edu.br/dicom/portal:latest;
+docker push hub.dds.ufvjm.edu.br/dicom/portal:latest 
 
 # docker build -t unixelias/plone.idg:1.1.5-dev \
 #         --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
