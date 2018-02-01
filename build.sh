66b509eff150699e0cb55a736a50ae54c323dde3
Elias Alves <unixelias@gmail.com>
2018-02-01 17:07:02 -0200

    Correção de tag

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
  #!/bin/bash
 shopt -s extglob
 
-branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+TAG=`git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,'`
 
-docker build -t unixelias/plone.idg:$branch \
+docker build -t unixelias/plone.idg:${TAG} \
                    --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
-                   --build-arg VCS_REF=`git rev-parse --short HEAD` docker/$branch
+                   --build-arg VCS_REF=`git rev-parse --short HEAD` docker/${TAG}
 
-docker push unixelias/plone.idg:$branch
+docker push unixelias/plone.idg:${TAG}
