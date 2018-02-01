d9582bf6119648cf5261797ffa014ac0038752ca
Elias Alves <unixelias@gmail.com>
2018-02-01 17:12:21 -0200

    Branch tag from Travis

--- build.sh
+++ build.sh
@@ -1,10 +1,8 @@
  #!/bin/bash
 shopt -s extglob
 
-TAG=`git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,'`
-
-docker build -t unixelias/plone.idg:${TAG} \
+docker build -t unixelias/plone.idg:${TRAVIS_BRANCH} \
                    --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
-                   --build-arg VCS_REF=`git rev-parse --short HEAD` docker/${TAG}
+                   --build-arg VCS_REF=`git rev-parse --short HEAD` docker/${TRAVIS_BRANCH}
 
-docker push unixelias/plone.idg:${TAG}
+docker push unixelias/plone.idg:${TRAVIS_BRANCH}
