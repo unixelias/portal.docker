12257c13a1dca667d72d5b5aeabfc21e80af0d6a
Elias Alves <unixelias@gmail.com>
2018-02-01 17:32:18 -0200

    Tags OLD
    Separação de tags antigas em outro Branch

--- build.sh
+++ build.sh
@@ -1,19 +1,11 @@
  #!/bin/bash
 shopt -s extglob
 
-last=""
-
 for i in $( ls docker); do
  case "$i" in
-      dev ) docker build -t unixelias/plone.idg:$i \
+      1.1.5.1|1.1.5.2|1.1.5.3|1.2|1.4 ) docker build -t unixelias/plone.idg:$i \
                    --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
                    --build-arg VCS_REF=`git rev-parse --short HEAD` docker/$i \
-                   && docker push unixelias/plone.idg:$i \
-                   && last=$i ;;
+                   && docker push unixelias/plone.idg:$i
  esac
 done
-
-# docker build -t unixelias/plone.idg:1.1.5-dev \
-#         --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
-#         --build-arg VCS_REF=`git rev-parse --short HEAD` docker/1.1.5-dev \
-#         && docker push unixelias/plone.idg:1.1.5-dev
--- scripts/volumes.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker volume create -d local-persist -o mountpoint=~/portal/data -o command="chown -R 500:500 /data" --name=data
