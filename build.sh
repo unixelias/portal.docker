34b38909582a47ff8c553dd1a7402a9d9545d27b
Elias Alves <unixelias@gmail.com>
2018-02-01 21:27:26 -0200

    Dockerfile único

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ shopt -s extglob
 
 docker build -t unixelias/plone.idg:${TRAVIS_BRANCH} \
                    --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
-                   --build-arg VCS_REF=`git rev-parse --short HEAD` docker/${TRAVIS_BRANCH}
+                   --build-arg VCS_REF=`git rev-parse --short HEAD` docker
 
 docker push unixelias/plone.idg:${TRAVIS_BRANCH};
