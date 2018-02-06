fd835dabc54a5501a5bf0fd0fd7a819f01ec66e9
Elias Alves <unixelias@gmail.com>
2018-02-06 18:25:02 -0200

    Refatora Dockerfile

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ shopt -s extglob
 
 docker build -t unixelias/plone.idg:${TRAVIS_BRANCH} \
                    --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
-                   --build-arg VCS_REF=`git rev-parse --short HEAD` docker
+                   --build-arg BRANCHE_VCS=${TRAVIS_BRANCH} \
+                   --build-arg VCS_REF=`git rev-parse --short HEAD` docker;                   
 
 docker push unixelias/plone.idg:${TRAVIS_BRANCH};
